ef9aa2872b4a37e29195da47a2ccaa220805ec36
angelezpinn <angelezpinn@gmail.com>
2024-04-25 00:10:32 -0600

    supabaseconection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './componentes/titulo';
 import Card from './componentes/aleCrm';
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://klvyjzzctlgdojqtvork.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsdnlqenpjdGxnZG9qcXR2b3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMjI3OTAsImV4cCI6MjAyODg5ODc5MH0.P6YayJKLBJ0BTg5NzTLGrW8-RVKL1s4sstiWUzTg9u0");
 
 import './App.css';
 
 function App() {
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(()=>{
+    getUsuarios();
+  },[]);
+
+  async function getUsuarios(){
+    const {data} = await supabase.from("usuarios").select();
+    setUsuarios(data);
+  }
+
   const cards = ['users', 'clients', 'products', 'sessions', 'categories', 'directions', 'genders', 'sessionsProducts'];
 
   function toTitleCase(str) {
@@ -13,6 +28,11 @@ function App() {
 
   return (
     <div>
+      <ul>
+        {usuarios.map((usuarios)=>(
+        <li key={usuarios.nombre}>{usuarios.nombre}</li>
+        ))}
+      </ul>
       <Header />
       <div className="container" id="cardContainer">
         {cards.map(cardName => (
